4bd13e7e32467e4aa7f4ae1300f522b84b49c6cf
Nam Phùng <namtotet205@gmail.com>
2024-02-27 02:43:04 +0700

    test renderer, no viewport

--- SDL2Game1/source.cpp
+++ SDL2Game1/source.cpp
@@ -11,7 +11,7 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
 
-SDL_Surface* loadedSurface = NULL;
+
 
 //Starts up SDL and creates window
 bool init();
@@ -22,48 +22,58 @@ bool loadMedia();
 //Frees media and shuts down SDL
 void close();
 
+//Loads individual image
+SDL_Surface* loadSurface(std::string path);
+
+//Loads individual image as texture
+SDL_Texture* loadTexture(std::string path);
+
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
 //The surface contained by the window
 SDL_Surface* gScreenSurface = NULL;
 
-//The image we will load and show on the screen
-SDL_Surface* gHelloWorld = NULL;
+//Current displayed PNG image
+SDL_Surface* gPNGSurface = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Current displayed texture
+SDL_Texture* gTexture = NULL;
 
 bool init()
 {
 	//Initialization flag
 	bool success = true;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	//Create window
+	gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (gWindow == NULL)
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 		success = false;
 	}
 	else
 	{
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
+		//Create renderer for window
+		gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+		if (gRenderer == NULL)
 		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
 		}
 		else
 		{
+			//Initialize renderer color
+			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
 			//Initialize PNG loading
 			int imgFlags = IMG_INIT_PNG;
 			if (!(IMG_Init(imgFlags) & imgFlags))
 			{
 				printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
 				success = false;
-			}
-			else
-			{
-				//Get window surface
-				gScreenSurface = SDL_GetWindowSurface(gWindow);
 			}
 		}
 	}
@@ -76,11 +86,11 @@ bool loadMedia()
 	//Loading success flag
 	bool success = true;
 
-	//Load splash image
-	gHelloWorld = SDL_LoadBMP("src/hello_world.bmp");
-	if (gHelloWorld == NULL)
+	//Load PNG texture
+	gTexture = IMG_LoadTexture(gRenderer,"src/slime.png");
+	if (gTexture == NULL)
 	{
-		printf("Unable to load image %s! SDL Error: %s\n", "src/hello_world.bmp", SDL_GetError());
+		printf("Failed to load texture image!\n");
 		success = false;
 	}
 
@@ -89,24 +99,28 @@ bool loadMedia()
 
 void close()
 {
-	//Deallocate surface
-	SDL_FreeSurface(gHelloWorld);
-	gHelloWorld = NULL;
+	//Free loaded image
+	SDL_DestroyTexture(gTexture);
+	gTexture = NULL;
 
-	//Destroy window
+	//Destroy window    
+	SDL_DestroyRenderer(gRenderer);
 	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
+	gRenderer = NULL;
 
 	//Quit SDL subsystems
+	IMG_Quit();
 	SDL_Quit();
 }
+
 SDL_Surface* loadSurface(std::string path)
 {
 	//The final optimized image
 	SDL_Surface* optimizedSurface = NULL;
 
 	//Load image at specified path
-	loadedSurface = IMG_Load(path.c_str());
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
 	if (loadedSurface == NULL)
 	{
 		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
@@ -137,21 +151,39 @@ int main(int argc, char* args[])
 	else
 	{
 		//Load media
-		if (!loadSurface("src/slime.png"))
+		if (!loadMedia())
 		{
 			printf("Failed to load media!\n");
 		}
 		else
 		{
-			loadSurface("src/slime.png");
-			//Apply the image
-			SDL_BlitSurface(loadedSurface, NULL, gScreenSurface, NULL);
+			//Main loop flag
+			bool quit = false;
 
-			//Update the surface
-			SDL_UpdateWindowSurface(gWindow);
+			//Event handler
+			SDL_Event e;
 
-			//Hack to get window to stay up
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+			//While application is running
+			while (!quit)
+			{
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0)
+				{
+					//User requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+				//Clear screen (background -> color in gRenderer) (giong nhu xoa background)
+				SDL_RenderClear(gRenderer);
+
+				//Render texture to screen
+				SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+				//Update screen
+				SDL_RenderPresent(gRenderer);
+			}
 		}
 	}
 
